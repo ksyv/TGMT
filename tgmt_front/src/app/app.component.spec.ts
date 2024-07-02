9d07519d26887b2a3e6db1dd8a67a252885c268a
ksyv <viais.kevin@hotmail.fr>
2024-07-02 11:25:34 +0200

    Reorganization: separation into two folders for the front (initialized with Angular) and for the back

--- tgmt_front/src/app/app.component.spec.ts
+++ tgmt_front/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tgmt'`, () => {
+  it(`should have as title 'tgmt_front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tgmt');
+    expect(app.title).toEqual('tgmt_front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tgmt');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tgmt_front');
   });
 });
