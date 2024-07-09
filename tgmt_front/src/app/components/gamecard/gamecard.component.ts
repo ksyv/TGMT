c423bd8f8596b7f1086f1d3b380ffdf1f413b351
ksyv <viais.kevin@hotmail.fr>
2024-07-09 14:34:13 +0200

    presentation de la page gamecard

--- tgmt_front/src/app/components/gamecard/gamecard.component.ts
+++ tgmt_front/src/app/components/gamecard/gamecard.component.ts
@@ -8,17 +8,15 @@ import { GameService } from '../../services/game.service'; // Assurez-vous d'imp
 })
 export class GamecardComponent implements OnInit {
   games: any[] = []; // Assurez-vous de définir le type correct ici
-  searchParams = {
-    name: '',
-    type: '',
-    category: '',
-    ageMin: null,
-    difficulty: '',
-    author: '',
-    playerMin: null,
-    playerMax: null,
-    partytime: null
-  };
+  searchName: string = '';
+  searchType: string = '';
+  searchCategory: string = '';
+  searchAgeMin: number | null = null;
+  searchDifficulty: string = '';
+  searchAuthor: string = '';
+  searchPlayerMin: number | null = null;
+  searchPlayerMax: number | null = null;
+  searchPartytime: number | null = null;
 
   constructor(private gameService: GameService) {}
 
@@ -26,7 +24,7 @@ export class GamecardComponent implements OnInit {
     this.fetchGames(); // Appelez la méthode pour récupérer les jeux lors de l'initialisation
   }
 
-  fetchGames() {
+  fetchGames(): void {
     this.gameService.getGames().subscribe(
       (data: any) => {
         this.games = data.result; // Assurez-vous que votre backend renvoie les jeux dans 'data.result'
@@ -37,10 +35,22 @@ export class GamecardComponent implements OnInit {
     );
   }
 
-  searchGames() {
-    this.gameService.searchGames(this.searchParams).subscribe(
+  onSearch(): void {
+    const searchCriteria = {
+      name: this.searchName,
+      type: this.searchType,
+      category: this.searchCategory,
+      ageMin: this.searchAgeMin,
+      difficulty: this.searchDifficulty,
+      author: this.searchAuthor,
+      playerMin: this.searchPlayerMin,
+      playerMax: this.searchPlayerMax,
+      partytime: this.searchPartytime
+    };
+
+    this.gameService.searchGames(searchCriteria).subscribe(
       (data: any) => {
-        this.games = data.result;
+        this.games = data.result; // Mettez à jour la liste des jeux en fonction des résultats de la recherche
       },
       (error: any) => {
         console.error('Error searching games:', error);
